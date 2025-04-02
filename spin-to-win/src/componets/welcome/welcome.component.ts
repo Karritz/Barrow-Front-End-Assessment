3c206f868ac601ee53c1812978a666742d1eede5
Gareth Williams <65289470+Karritz@users.noreply.github.com>
2025-04-02 10:44:56 +0200

    code tidy up

--- spin-to-win/src/componets/welcome/welcome.component.ts
+++ spin-to-win/src/componets/welcome/welcome.component.ts
@@ -9,13 +9,11 @@ import { Router } from '@angular/router';
 })
 export class WelcomeComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router) {}
 
+  navigateToGame() {
+    this.router.navigate(['/game']);
   }
-
-navigateToGame() {
-  this.router.navigate(['/game']);
-}
 
 
 }
