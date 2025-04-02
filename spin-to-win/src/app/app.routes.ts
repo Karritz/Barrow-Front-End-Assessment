e51f5574370d0688a34262295c4efec029ad210d
Gareth Williams <65289470+Karritz@users.noreply.github.com>
2025-04-02 10:41:16 +0200

    added routing componets

--- spin-to-win/src/app/app.routes.ts
+++ spin-to-win/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { SpinningWheelGameComponent } from '../componets/spinning-wheel-game/spinning-wheel-game.component';
+import { ResultsComponent } from '../componets/results/results.component';
+import { WelcomeComponent } from '../componets/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/welcome',
+        pathMatch: 'full'
+    },
+    {
+        path: 'game', component: SpinningWheelGameComponent
+    },
+    {
+        path: 'result', component: ResultsComponent
+    },
+    {
+        path: 'welcome', component: WelcomeComponent
+    },
+    {
+        path: '**' , component: WelcomeComponent
+    }
+];
