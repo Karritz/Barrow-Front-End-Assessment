9ed841e3c2adb521dcb7a60311cc191f72c6e28e
Gareth Williams <65289470+Karritz@users.noreply.github.com>
2025-04-02 10:44:15 +0200

    Router works

--- spin-to-win/src/componets/welcome/welcome.component.ts
+++ spin-to-win/src/componets/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+navigateToGame() {
+  this.router.navigate(['/game']);
+}
+
 
 }
